e1bdb2a839da54daf5eb93e1696116c312ff5a1b
Nam <trungnam2101998@gmail.com>
2018-08-14 10:23:59 +0700

    cv2

---
+++ nampt/cv2/application/views/templates/master.php
@@ -0,0 +1,4 @@
+<?php $this->load->view('templates/header'); ?>
+<?php $this->load->view('templates/sidebar'); ?>
+<?php $this->load->view($content); ?>
+<?php $this->load->view('templates/footer'); ?>
