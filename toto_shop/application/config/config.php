83c84da3bb8750283f29665dd04eb7c63ee61b9f
lvtung8864 <FTWD1801010@st.fasttrack.edu.vn>
2018-09-14 19:25:12 +0700

    ssss

--- toto_shop/application/config/config.php
+++ toto_shop/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/www/CodeIgniter/';
+$config['base_url'] = 'http://localhost/FTWD1801.PHPWeb/toto_shop/';
 
 /*
 |--------------------------------------------------------------------------
--- toto_shop/application/views/templates/admin/master.php
+++
@@ -1,3 +0,0 @@
-<?php $this->load->view("templates/admin/header")?>
-<?php $this->load->view($content)?>
-<?php $this->load->view("templates/admin/footer")?>
