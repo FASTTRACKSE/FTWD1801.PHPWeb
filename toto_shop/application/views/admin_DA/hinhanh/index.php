b012ecb02cde8e4070bd3eb6328e993e60a6ac0b
Võ Thị Hoài <vothihoai170798@gmail.com>
2018-09-18 23:46:43 +0700

    toto_shop

--- toto_shop/application/views/admin_DA/hinhanh/index.php
+++ toto_shop/application/views/admin_DA/hinhanh/index.php
@@ -26,9 +26,9 @@
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
-                            <strong class="card-title">Tin tức</strong>
+                            <strong class="card-title">Hình ảnh</strong>
                         </div>
-                        <a href="<?=base_url("admin_DA/tintuc/add")?>"><button  class="btn btn-primary btn-lg" style="width: 60px;height: 40px;margin-left: 30px;margin-top: 10px;" > thêm</button></a>
+                        <a href="<?=base_url("index.php/admin_DA/hinhanh/add")?>"><button  class="btn btn-primary btn-lg" style="width: 60px;height: 40px;margin-left: 30px;margin-top: 10px;" > thêm</button></a>
                         <br>
                         <div class="row" style="margin-left: 20px;">
                             <form action="" method="GET">
@@ -40,19 +40,20 @@
                   <div id="bootstrap-data-table_wrapper" class="dataTables_wrapper container-fluid dt-bootstrap4 no-footer"><div class="row"><div class="col-sm-12 col-md-6"><div class="dataTables_length" id="bootstrap-data-table_length"><div id="bootstrap-data-table_filter" class="dataTables_filter"></div></div></div><div class="row"><div class="col-sm-12"><table id="bootstrap-data-table" class="table table-striped table-bordered dataTable no-footer" role="grid" aria-describedby="bootstrap-data-table_info">
                     <thead>
                       <tr role="row">
-                        <th class="sorting" tabindex="0" aria-controls="bootstrap-data-table" rowspan="1" colspan="1" aria-label="Office: activate to sort column ascending" style="width: 148px;" name="noidung">Nội dung</th>
-                      <th class="sorting" tabindex="0" aria-controls="bootstrap-data-table" rowspan="1" colspan="1" aria-label="Salary: activate to sort column ascending" style="width: 117px;"><a href="">Hoạt Động</th>
+                        <th class="sorting" tabindex="0" aria-controls="bootstrap-data-table" rowspan="1" colspan="1" aria-label="Position: activate to sort column ascending" style="width: 337px;" name="id">id</th>
+                        <th class="sorting" tabindex="0" aria-controls="bootstrap-data-table" rowspan="1" colspan="1" aria-label="Office: activate to sort column ascending" style="width: 148px;" name="image" value="">image</th>
+                      <th class="sorting" tabindex="0" aria-controls="bootstrap-data-table" rowspan="1" colspan="1" aria-label="Salary: activate to sort column ascending" style="width: 117px;"><a href="">hoạt động</th>
                       </tr>
                     </thead>
                     <tbody> 
-                      <?php foreach ( $tintuc as $item =>$value ) {?>
+                      <?php foreach ( $hinhanh as $item ) {?>
                     <tr role="row" class="odd">
 
-                        <td class="sorting_1"><?php echo $value["noidung"] ?></td>
-                                            
+                        <td class="sorting_1"><?php echo $item["id"] ?></td>
+                        <td ><img src="../../../uploads/<?= $item['image']?>"></td>                 
                         <td>
-                            <a href="<?=base_url("admin_DA/tintuc/edit/").$value['id']?>">sửa</a> ||
-                            <a href="<?=base_url("admin_DA/tintuc/delete/{$value['id']}")?>">xóa</a> 
+                            <a href="<?=base_url("index.php/admin_DA/hinhanh/edit/").$item['id']?>">sửa</a> ||
+                            <a href="<?=base_url("index.php/admin_DA/hinhanh/delete/{$item['id']}")?>">xóa</a> 
                         </td>
                         </tr>
                        <?php }?>
